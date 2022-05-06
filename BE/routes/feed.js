9f67af6838fa6750300fd3869393f864d39d5841
wahyuPM <wahyupratama191@gmail.com>
2022-05-07 00:03:17 +0800

    add : auth middleware to all route

--- BE/routes/feed.js
+++ BE/routes/feed.js
@@ -2,25 +2,26 @@ const express = require('express');
 const { body } = require('express-validator/check');
 
 const feedController = require('../controllers/feed');
+const isAuth = require('../middleware/is-auth');
 
 const router = express.Router();
 
 // GET /feed/posts
-router.get('/posts', feedController.getPosts);
+router.get('/posts', isAuth, feedController.getPosts);
 
 // POST /feed/post
-router.post('/post', [
+router.post('/post', isAuth, [
     body('title').trim().isLength({ min: 5 }).withMessage('Title must be at least 5 characters long'),
     body('content').trim().isLength({ min: 5 }).withMessage('Content must be at least 5 characters long')
 ], feedController.createPost);
 
-router.get('/post/:postId', feedController.getPostById);
+router.get('/post/:postId', isAuth, feedController.getPostById);
 
-router.put('/post/:postId', [
+router.put('/post/:postId', isAuth, [
     body('title').trim().isLength({ min: 5 }).withMessage('Title must be at least 5 characters long'),
     body('content').trim().isLength({ min: 5 }).withMessage('Content must be at least 5 characters long')
 ], feedController.updatePost);
 
-router.delete('/post/:postId', feedController.deletePost);
+router.delete('/post/:postId', isAuth, feedController.deletePost);
 
 module.exports = router;
